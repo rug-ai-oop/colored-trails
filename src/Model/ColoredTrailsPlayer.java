992df6fd48e145521cfb81fb2b08f637a24b4c1c
mBenescu <m.benescu@rug.student.nl>
2023-11-29 14:01:36 +0100

    Token class + added the hasReceivedSignal flag

--- src/Model/ColoredTrailsPlayer.java
+++ src/Model/ColoredTrailsPlayer.java
@@ -3,17 +3,25 @@ package Model;
 import java.util.ArrayList;
 
 public abstract class ColoredTrailsPlayer {
+    private boolean hasReceivedSignal;
     private Patch goal;
     private ArrayList tokens;
     private Patch currentPatch;
+    public ColoredTrailsPlayer(Patch goal) {
+        this.goal = goal;
+        hasReceivedSignal = false;
+    }
     public abstract void initiateOffer();
     public abstract void acceptOffer();
     public abstract void rejectOffer();
-    public abstract void communicateGoal();
+    public abstract Patch communicateGoal(ColoredTrailsPlayer otherPlayer);
     public abstract void moveToPatch();
 
-    public ColoredTrailsPlayer(Patch goal) {
-        this.goal = goal;
+    public void setHasReceivedSignal(boolean hasReceivedSignal) {
+        this.hasReceivedSignal = hasReceivedSignal;
     }
 
+    public ArrayList<> getTokens() {
+        return ( (ArrayList) tokens.clone() );
+    }
 }
---
+++ src/Model/HumanPlayer.java
@@ -0,0 +1,32 @@
+package Model;
+
+public class HumanPlayer extends ColoredTrailsPlayer{
+
+    public HumanPlayer(Patch goal) {
+        super(goal);
+    }
+
+    @Override
+    public void initiateOffer() {
+
+    }
+
+    @Override
+    public void acceptOffer() {
+
+    }
+
+    @Override
+    public void rejectOffer() {
+
+    }
+
+    @Override
+    public Patch communicateGoal(ColoredTrailsPlayer otherPlayer) {
+        return null;
+    }
+    @Override
+    public void moveToPatch() {
+
+    }
+}
