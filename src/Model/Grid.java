aa0e1bdfcecd7a1f247d1ebbb961897221e48ec9
LukaszSawala <lukaszsawala2003@gmail.com>
2023-11-29 23:53:05 +0100

    added patch functionality

--- src/Model/Grid.java
+++ src/Model/Grid.java
@@ -1,2 +1,12 @@
 package Model;
 import java.util.ArrayList;
+
+public class Grid {
+    private ArrayList<Patch> patches;
+    public Grid(ArrayList<Patch> patches) {
+        this.patches = patches;
+    }
+    public ArrayList<Patch> getPatches() {
+        return patches;
+    }
+}
