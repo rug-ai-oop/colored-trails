7dd2d480cee921afadf9715b10dfa4b6fe385bda
LukaszSawala <lukaszsawala2003@gmail.com>
2023-11-29 21:28:04 +0100

    added patch functionality

--- src/Model/Patch.java
+++ src/Model/Patch.java
@@ -1,4 +1,5 @@
 package Model;
+import java.util.ArrayList;
 
 public class Patch {
     private Color color;
@@ -6,4 +7,26 @@ public class Patch {
     public Color getColor() {
         return color;
     }
+
+    public void moveToPatch(ArrayList<Token> tokens, Patch goal, Patch current, ColoredTrailsPlayer player) {
+        ArrayList<Token> tokensToSpend = new ArrayList<Token>; //no clue how to work with that, lets try
+        ArrayList<Token> tokensLeft = tokens;
+        //we have to make an assumption that the player moves only to its neighbour, otherwise this violates the rules
+
+
+
+
+        if(tokensLeft.equals(tokens)) {
+            //umm whats there reverse of equals
+        }
+        else
+        {
+            player.currentPatch = goal;
+            //set tokens
+        }
+
+
+    }
+
+
 }
