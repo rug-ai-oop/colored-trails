522da26a39f8d38bf7ce8bf84610c6da3c78adc4
LukaszSawala <lukaszsawala2003@gmail.com>
2023-12-01 10:46:25 +0100

    added players to the grid

--- src/Model/Grid.java
+++ src/Model/Grid.java
@@ -1,13 +1,21 @@
 package Model;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Grid {
     private ArrayList<Patch> patches;
-    public Grid(ArrayList<Patch> patches) {
+    private ArrayList<ColoredTrailsPlayer> players;
+
+    public Grid(ArrayList<Patch> patches, ArrayList<ColoredTrailsPlayer> players) {
         this.patches = patches;
+        this.players = players;
     }
+
     public ArrayList<Patch> getPatches() {
         return patches;
     }
 
+    public ArrayList<ColoredTrailsPlayer> getPlayers() {
+        return players;
+    }
 }
